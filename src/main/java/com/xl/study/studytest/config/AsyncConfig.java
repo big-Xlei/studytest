29773428f4c65bc13f300cabc4ab2f5cf422af92
xionglei <xionglei@metrodata.cn>
2021-03-30 14:53:20 +0800

    添加http请求utils

---
+++ src/main/java/com/xl/study/studytest/config/AsyncConfig.java
@@ -0,0 +1,34 @@
+package com.xl.study.studytest.config;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig implements AsyncConfigurer {
+    @Override
+    public Executor getAsyncExecutor() {
+        // 定义线程池
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        // 核心线程数
+        taskExecutor.setCorePoolSize(10);
+        // 线程池最大线程数
+        taskExecutor.setMaxPoolSize(20);
+        // 线程队列最大线程数
+        taskExecutor.setQueueCapacity(2000);
+        // 初始化
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler(){
+        return null;
+    }
+
+}
